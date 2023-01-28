6c4307b0d049d0fc3b41b21c9a32d2d46f22451e
AngKhaiPin <1211101248@student.mmu.edu.my>
2023-01-28 13:16:11 +0800

    new file:   ass test.cpp 	new file:   ass test.exe 	new file:   main.cpp 	new file:   main.exe 	new file:   pf/header.cpp 	new file:   pf/header.h 	new file:   pf/helper.h.gch 	new file:   pf/randomiser.cpp

---
+++ main.cpp
@@ -0,0 +1,165 @@
+// *********************************************************
+// Course: TCP1101 PROGRAMMING FUNDAMENTALS
+// Year: Trimester 1, 2022/23 (T2215)
+// Lab: TT1L
+// Names: ANG KHAI PIN | LESTER LIEW JUN MIN |
+// IDs: 1211101248 | 1221302721 |
+// Emails: 1211101248@student.mmu.edu.my | 1221302721@student.mmu.edu.my |
+// Phones: 011-57725120 | 012-9156393 |
+// *********************************************************
+
+#include "pf/header.h"
+#include <algorithm>
+
+char GSchoice;
+int Rows = 3, Columns = 9;
+
+std::vector<std::vector<char>> board; // Make the board a sort of matrix
+int kColumns = (Columns * 2) + 1;
+int XCount = 1;
+
+Player Alien;
+Enemy Zombie;
+Map map;
+
+template <typename T> // Overloading Operator "<<" to let std::cout print out vector. (MUST NOT TOUCH)
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &v)
+{
+
+    for (size_t i = v.size() - 1; i < v.size(); i++)
+    {
+        os << v[i];
+    }
+    return os;
+}
+
+void Pause()
+{
+    pf::Pause();
+    std::cout << std::endl;
+}
+
+void ClearScreen()
+{
+    pf::ClearScreen();
+    std::cout << std::endl;
+}
+
+void ChangeZombieSettings()
+{
+    Sleep(500);
+    std::cout << "\nZombie Settings\n";
+    std::cout << "-----------------\n";
+    std::cout << "Enter number of zombies: ";
+    std::cin >> Zombie.ZombieCount;
+    if (Zombie.ZombieCount >= 10)
+    {
+        std::cout << "Number of zombies cannot exceed 9.\n";
+        Sleep(3000);
+        ClearScreen();
+        ChangeZombieSettings();
+    }
+    else
+    {
+        std::cout << "\nSettings Updated.\n";
+        Pause();
+    }
+}
+
+void ChangeGameSettings()
+{
+    std::cout << "Board Settings\n";
+    std::cout << "-----------------\n";
+    std::cout << "Enter Rows    : ";
+    std::cin >> Columns;
+    std::cout << "Enter Columns : ";
+    std::cin >> Rows;
+    if (Rows % 2 == 0 || Columns % 2 == 0)
+    {
+        std::cout << "Please ensure that Rows and Columns are odd numbers.\n";
+        Sleep(4000);
+        ClearScreen();
+        ChangeGameSettings();
+    }
+    else
+    {
+        ChangeZombieSettings();
+    }
+}
+
+void GameSettings()
+{
+    std::cout << "Board Rows    : " << Rows << '\n';
+    std::cout << "Board Columns : " << Columns << '\n';
+    std::cout << "Zombie Count  : " << Zombie.ZombieCount;
+    std::cout << "\nDo you wish to change the game settings (y/n)? => ";
+    std::cin >> GSchoice;
+    if (GSchoice == 'y' || GSchoice == 'Y')
+    {
+        ClearScreen();
+        ChangeGameSettings();
+    }
+}
+
+void ShowGameSettings()
+{
+    ClearScreen();
+    std::cout << "Default Game Settings\n";
+    std::cout << "-------------------------\n";
+    GameSettings();
+}
+
+void makeBoard()
+{
+    map.init(Rows, Columns);
+    Alien.InitialLanding(map, Rows, Columns);
+    // pf::ClearScreen();
+    map.display();
+    Alien.newPosX_ = Alien.posX_;
+    Alien.newPosY_ = Alien.posY_;
+}
+
+void ShowAlienHUD()
+{
+    std::cout << "\n->Alien    : Life " << Alien.AlienHp << ", Attack  " << Alien.AlienAtk;
+}
+
+void ShowZombieHUD()
+{
+    Zombie.ZombieCreation();
+}
+
+void PlayerMovement()
+{
+    std::cout << std::endl;
+    std::cout << "Command => ";
+    std::string userInput;
+    std::cin >> userInput;
+    do
+    {
+        Alien.AlienMove(map, userInput, Rows, Columns);
+        Alien.AlienPlacement(map);
+        pf::ClearScreen();
+        map.display();
+        if (Alien.hitBarrier == true)
+        {
+            std::cout << "Alien hit the barrier!" << std::endl;
+            pf::Pause();
+        }
+        if (Alien.hitObject == true)
+        {
+            std::cout << "Alien hit an object!" << std::endl;
+            pf::Pause();
+        }
+    } while (Alien.hitBarrier == false && Alien.hitObject == false);
+}
+
+int main()
+{
+    ShowGameSettings();
+    makeBoard();
+    ShowAlienHUD();
+    ShowZombieHUD();
+    PlayerMovement();
+    PlayerMovement();
+}
---
+++ pf/header.cpp
@@ -0,0 +1,532 @@
+#include "header.h"
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+
+// g++ pf\*.cpp main.cpp -o main
+
+void Map::init(int rows, int columns)
+{
+    rows_ = rows;
+    columns_ = columns;
+    char randobjects[] = {' ', ' ', ' ', ' ', ' ', ' ', 'h', 'v', '^', '<', '>', 'p'};
+    int noOfObjects = 12; // number of objects in the objects array
+    // create dynamic 2D array using vector
+    map_.resize(columns_); // create empty rows
+    for (int i = 0; i < columns_; ++i)
+    {
+        map_[i].resize(rows_); // resize each row
+    }
+    // put random characters into the vector array
+    for (int i = 0; i < columns_; ++i)
+    {
+        for (int j = 0; j < rows_; ++j)
+        {
+            int objNo = rand() % noOfObjects;
+            map_[i][j] = randobjects[objNo];
+        }
+    }
+}
+
+void Map::display() const
+{
+    std:: cout << ".: Alien vs Zombie :." << std::endl;
+
+    // Prints out each row
+    for (int i = 0; i < columns_; ++i)
+    {
+        // Displays border for top row
+        std::cout << "  ";
+        for (int j = 0; j < rows_; ++j)
+        {
+            std::cout << "+-";
+        }
+        std::cout << "+" << std::endl;
+        // Displays Row Number
+        std::cout << std::setw(2) << (i + 1);
+        // Displays the vector contents and border in between them
+        for (int j = 0; j < rows_; ++j)
+        {
+            std::cout << "|" << map_[i][j];
+        }
+        std::cout << "|" << std::endl;
+    }
+    // Display the border for the last row
+    std::cout << "  ";
+    for (int j = 0; j < rows_; ++j)
+    {
+        std::cout << "+" << "-";
+    }
+    std::cout << "+" << std::endl;
+    // display column number
+    std::cout << "  ";
+    for (int j = 0; j < rows_; ++j)
+    {
+        int digit = (j + 1) / 10;
+        std::cout << " ";
+        if (digit == 0)
+            std::cout << " ";
+        else
+            std::cout << digit;
+    }
+    std::cout << std::endl;
+    std::cout << "  ";
+    for (int j = 0; j < rows_; ++j)
+    {
+        std::cout << " " << (j + 1) % 10;
+    }
+    std::cout << std::endl;
+}
+
+char Map::getObject(int x, int y) const
+{
+    return map_[y - 1][x - 1];
+}
+
+void Map::setObject(int x, int y, char ch)
+{
+    map_[y - 1][x - 1] = ch;
+}
+
+namespace pf
+{
+    int ClearScreen()
+    {
+        #if defined(_WIN32)
+            return std::system("cls");
+        #elif defined(__linux__) || defined(__APPLE__)
+            return std::system("clear");
+        #endif
+    }
+
+    int Pause()
+    {
+        #if defined(_WIN32)
+            return std::system("pause");
+        #elif defined(__linux__) || defined(__APPLE__)
+            return std::system(R"(read -p "Press any key to continue . . . " dummy)");
+        #endif
+    }
+}
+
+void Player::InitialLanding(Map &map_, float x, float y)
+{
+    float middleX, middleY;
+    //Get middle positions based on the size of map
+    middleX = ceil(x / 2);
+    middleY = ceil(y / 2);
+    posX_ = middleX;
+    posY_ = middleY;
+
+    map_.setObject(middleX, middleY, AlienSymbol);
+}
+
+void Player::AlienMove(Map &map_, std::string inp, int x, int y)
+{
+        if (inp == "Up" || inp == "up" || inp == "UP")
+        {
+            if (newPosY_ == 1)
+            {
+                hitBarrier = true;
+            }
+            else
+            {
+                hitBarrier = false;
+                if (map_.getObject(posX_, posY_ - 1) == 'r')
+                {
+                    hitObject = true;
+                }
+                if (map_.getObject(posX_, posY_ - 1) == '^')
+                {
+                    hitObject = false;
+                    newPosX_ = posX_;
+                    newPosY_ = posY_ - 1;
+                    inp = "up";
+                    do
+                    {
+                        AlienMove(map_, inp, x, y);
+                        AlienPlacement(map_);
+                        pf::ClearScreen();
+                        map_.display();
+                        if (hitBarrier == true)
+                        {
+                            std::cout << "Alien hit the barrier!" << std::endl;
+                            pf::Pause();
+                        }
+                        if (hitObject == true)
+                        {
+                            std::cout << "Alien hit an object!" << std::endl;
+                            pf::Pause();
+                        }
+                    } while (hitBarrier == false && hitObject == false);
+                }
+                if (map_.getObject(posX_, posY_ - 1) == 'v')
+                {
+                    hitObject = false;
+                    newPosX_ = posX_;
+                    newPosY_ = posY_ - 1;
+                    inp = "down";
+                    do
+                    {
+                        AlienMove(map_, inp, x, y);
+                        AlienPlacement(map_);
+                        pf::ClearScreen();
+                        map_.display();
+                        if (hitBarrier == true)
+                        {
+                            std::cout << "Alien hit the barrier!" << std::endl;
+                            pf::Pause();
+                        }
+                        if (hitObject == true)
+                        {
+                            std::cout << "Alien hit an object!" << std::endl;
+                            pf::Pause();
+                        }
+                    } while (hitBarrier == false && hitObject == false);
+                }
+                if (map_.getObject(posX_, posY_ - 1) == '<')
+                {
+                    hitObject = false;
+                    newPosX_ = posX_;
+                    newPosY_ = posY_ - 1;
+                    inp = "left";
+                    do
+                    {
+                        AlienMove(map_, inp, x, y);
+                        AlienPlacement(map_);
+                        pf::ClearScreen();
+                        map_.display();
+                        if (hitBarrier == true)
+                        {
+                            std::cout << "Alien hit the barrier!" << std::endl;
+                            pf::Pause();
+                        }
+                        if (hitObject == true)
+                        {
+                            std::cout << "Alien hit an object!" << std::endl;
+                            pf::Pause();
+                        }
+                    } while (hitBarrier == false && hitObject == false);
+                }
+                if (map_.getObject(posX_, posY_ - 1) == '>')
+                {
+                    hitObject = false;
+                    newPosX_ = posX_;
+                    newPosY_ = posY_ - 1;
+                    inp = "right";
+                    do
+                    {
+                        AlienMove(map_, inp, x, y);
+                        AlienPlacement(map_);
+                        pf::ClearScreen();
+                        map_.display();
+                        if (hitBarrier == true)
+                        {
+                            std::cout << "Alien hit the barrier!" << std::endl;
+                            pf::Pause();
+                        }
+                        if (hitObject == true)
+                        {
+                            std::cout << "Alien hit an object!" << std::endl;
+                            pf::Pause();
+                        }
+                    } while (hitBarrier == false && hitObject == false);
+                }
+                // //
+                else
+                {
+                    hitObject = false;
+                    newPosX_ = posX_;
+                    newPosY_ = posY_ - 1;
+                }
+            }
+        }
+        if (inp == "down" || inp == "Down" || inp == "DOWN")
+        {
+            if (newPosY_ == y)
+            {
+                hitBarrier = true;
+            }
+            else
+            {
+                hitBarrier = false;
+                if (map_.getObject(posX_, posY_ + 1) == 'r')
+                {
+                    hitObject = true;
+                }
+                if (map_.getObject(posX_, posY_ + 1) == '^')
+                {
+                    hitObject = false;
+                    newPosX_ = posX_;
+                    newPosY_ = posY_ + 1;
+                    inp = "up";
+                    do
+                    {
+                        AlienMove(map_, inp, x, y);
+                        AlienPlacement(map_);
+                        pf::ClearScreen();
+                        map_.display();
+                        if (hitBarrier == true)
+                        {
+                            std::cout << "Alien hit the barrier!" << std::endl;
+                            pf::Pause();
+                        }
+                        if (hitObject == true)
+                        {
+                            std::cout << "Alien hit an object!" << std::endl;
+                            pf::Pause();
+                        }
+                    } while (hitBarrier == false && hitObject == false);
+                }
+                if (map_.getObject(posX_, posY_ + 1) == 'v')
+                {
+                    hitObject = false;
+                    newPosX_ = posX_;
+                    newPosY_ = posY_ + 1;
+                    inp = "down";
+                    do
+                    {
+                        AlienMove(map_, inp, x, y);
+                        AlienPlacement(map_);
+                        pf::ClearScreen();
+                        map_.display();
+                        if (hitBarrier == true)
+                        {
+                            std::cout << "Alien hit the barrier!" << std::endl;
+                            pf::Pause();
+                        }
+                        if (hitObject == true)
+                        {
+                            std::cout << "Alien hit an object!" << std::endl;
+                            pf::Pause();
+                        }
+                    } while (hitBarrier == false && hitObject == false);
+                }
+                if (map_.getObject(posX_, posY_ + 1) == '<')
+                {
+                    hitObject = false;
+                    newPosX_ = posX_;
+                    newPosY_ = posY_ + 1;
+                    inp = "left";
+                    do
+                    {
+                        AlienMove(map_, inp, x, y);
+                        AlienPlacement(map_);
+                        pf::ClearScreen();
+                        map_.display();
+                        if (hitBarrier == true)
+                        {
+                            std::cout << "Alien hit the barrier!" << std::endl;
+                            pf::Pause();
+                        }
+                        if (hitObject == true)
+                        {
+                            std::cout << "Alien hit an object!" << std::endl;
+                            pf::Pause();
+                        }
+                    } while (hitBarrier == false && hitObject == false);
+                }
+                if (map_.getObject(posX_, posY_ + 1) == '>')
+                {
+                    hitObject = false;
+                    newPosX_ = posX_;
+                    newPosY_ = posY_ + 1;
+                    inp = "right";
+                    do
+                    {
+                        AlienMove(map_, inp, x, y);
+                        AlienPlacement(map_);
+                        pf::ClearScreen();
+                        map_.display();
+                        if (hitBarrier == true)
+                        {
+                            std::cout << "Alien hit the barrier!" << std::endl;
+                            pf::Pause();
+                        }
+                        if (hitObject == true)
+                        {
+                            std::cout << "Alien hit an object!" << std::endl;
+                            pf::Pause();
+                        }
+                    } while (hitBarrier == false && hitObject == false);
+                }
+                else
+                {
+                    hitObject = false;
+                    newPosX_ = posX_;
+                    newPosY_ = posY_ + 1;
+                }
+            }
+        }
+        if (inp == "Left" || inp == "left" || inp == "LEFT")
+        {
+            if (newPosX_ == 1)
+            {
+                hitBarrier = true;
+            }
+            else
+            {
+                hitBarrier = false;
+                if (map_.getObject(posX_ - 1, posY_) == 'r')
+                {
+                    hitObject = true;
+                }
+                if (map_.getObject(posX_ - 1, posY_) == '^')
+                {
+                    hitObject = false;
+                    newPosX_ = posX_-1;
+                    newPosY_ = posY_;
+                    AlienPlacement(map_);
+                    inp = "up";
+                    do
+                    {
+                        AlienMove(map_, inp, x, y);
+                        AlienPlacement(map_);
+                        pf::ClearScreen();
+                        map_.display();
+                        if (hitBarrier == true)
+                        {
+                            std::cout << "Alien hit the barrier!" << std::endl;
+                            pf::Pause();
+                        }
+                        if (hitObject == true)
+                        {
+                            std::cout << "Alien hit an object!" << std::endl;
+                            pf::Pause();
+                        }
+                    } while (hitBarrier == false && hitObject == false);
+                }
+                if (map_.getObject(posX_ - 1, posY_) == 'v')
+                {
+                    hitObject = false;
+                    newPosX_ = posX_-1;
+                    newPosY_ = posY_;
+                    AlienPlacement(map_);
+                    inp = "down";
+                    do
+                    {
+                        AlienMove(map_, inp, x, y);
+                        AlienPlacement(map_);
+                        pf::ClearScreen();
+                        map_.display();
+                        if (hitBarrier == true)
+                        {
+                            std::cout << "Alien hit the barrier!" << std::endl;
+                            pf::Pause();
+                        }
+                        if (hitObject == true)
+                        {
+                            std::cout << "Alien hit an object!" << std::endl;
+                            pf::Pause();
+                        }
+                    } while (hitBarrier == false && hitObject == false);
+                }
+                if (map_.getObject(posX_ - 1, posY_) == '<')
+                {
+                    hitObject = false;
+                    newPosX_ = posX_-1;
+                    newPosY_ = posY_;
+                    AlienPlacement(map_);
+                    inp = "left";
+                    do
+                    {
+                        AlienMove(map_, inp, x, y);
+                        AlienPlacement(map_);
+                        pf::ClearScreen();
+                        map_.display();
+                        if (hitBarrier == true)
+                        {
+                            std::cout << "Alien hit the barrier!" << std::endl;
+                            pf::Pause();
+                        }
+                        if (hitObject == true)
+                        {
+                            std::cout << "Alien hit an object!" << std::endl;
+                            pf::Pause();
+                        }
+                    } while (hitBarrier == false && hitObject == false);
+                }
+                if (map_.getObject(posX_ - 1, posY_) == '>')
+                {
+                    hitObject = false;
+                    newPosX_ = posX_-1;
+                    newPosY_ = posY_;
+                    AlienPlacement(map_);
+                    inp = "right";
+                    do
+                    {
+                        AlienMove(map_, inp, x, y);
+                        AlienPlacement(map_);
+                        pf::ClearScreen();
+                        map_.display();
+                        if (hitBarrier == true)
+                        {
+                            std::cout << "Alien hit the barrier!" << std::endl;
+                            pf::Pause();
+                        }
+                        if (hitObject == true)
+                        {
+                            std::cout << "Alien hit an object!" << std::endl;
+                            pf::Pause();
+                        }
+                    } while (hitBarrier == false && hitObject == false);
+                }
+                else
+                {
+                    hitObject = false;
+                    newPosX_ = posX_ - 1;
+                    newPosY_ = posY_;
+                }
+            }
+        }
+        if (inp == "Right" || inp == "right" || inp == "RIGHT")
+        {
+            if (newPosX_ == x)
+            {
+                hitBarrier = true;
+            }
+            else
+            {
+                hitBarrier = false;
+                if (map_.getObject(posX_ + 1, posY_) == 'r')
+                {
+                    hitObject = true;
+                }
+                else
+                {
+                    hitObject = false;
+                    newPosX_ = posX_ + 1;
+                    newPosY_ = posY_;
+                }
+            }
+        }    
+}
+
+void Player::AlienPlacement(Map &map_)
+{
+    if (hitBarrier == false && hitObject == false)
+    {
+        map_.setObject(newPosX_, newPosY_, AlienSymbol);
+        map_.setObject(posX_, posY_, '.');
+        posX_ = newPosX_;
+        posY_ = newPosY_;
+    }
+}
+
+void Enemy::ZombieCreation()
+{
+    for (int i = 0; i < ZombieCount; i++)
+    {
+        int randomHp = rand() % 2 + 1;
+        int randomAtk = rand() % 2 + 1;
+        int randomRng = rand() % 5 + 1;
+        int ZombieHp = 100 + (randomHp * 50);
+        int ZombieAtk = 5 + (randomAtk * 5);
+        int ZombieRange = randomRng;
+        ZombHpVec.push_back(ZombieHp);
+        ZombAtkVec.push_back(ZombieAtk);
+        ZombRngVec.push_back(ZombieRange);
+        std::cout << '\n' << "  Zombie " << i + 1 << " : Life " << ZombieHp << ", Attack  " << ZombieAtk << ", Range " << ZombieRange;
+    }
+}
+
