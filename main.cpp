ea401af7a3e6f50e3a6949efe330aede17e40817
AngKhaiPin <1211101248@student.mmu.edu.my>
2023-01-28 16:13:20 +0800

    Changes to be committed:

--- main.cpp
+++ main.cpp
@@ -109,7 +109,7 @@ void ShowGameSettings()
     GameSettings();
 }
 
-void MakeBoard()
+void makeBoard()
 {
     map.init(Rows, Columns);
     Alien.InitialLanding(map, Rows, Columns);
@@ -157,7 +157,7 @@ void PlayerMovement()
 int main()
 {
     ShowGameSettings();
-    MakeBoard();
+    makeBoard();
     ShowAlienHUD();
     ShowZombieHUD();
     PlayerMovement();
